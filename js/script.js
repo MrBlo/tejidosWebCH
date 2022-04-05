a172dd047d0a949f7f531ef8b147b7e8d17681ca
Pablo Arias <pabloarias.ic@gmail.com>
2022-04-04 22:21:23 -0400

    Final push

--- js/script.js
+++
@@ -1,6 +0,0 @@
-const element = document.querySelector('.main__feature');
-element.classList.add('animate__animated', 'animate__bounceIn');
-element.onclick = () => {
-    element.classList.add('animate__animated', 'animate__bounceIn');
-    console.log("clicked");
-};
